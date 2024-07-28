ecb4270306f9fe099d769ac3c774b1f6af0347ce
ravana09 <pradeep09sathish@gmail.com>
2024-07-28 11:59:00 +0530

    "Updated package-lock.json, package.json, and several component files (About.js, Header.js) with new dependencies and code changes."

--- src/components/About.js
+++ src/components/About.js
@@ -1,66 +1,107 @@
-import React from 'react';
-import '../App.css';
-import AboutImage from'../assests/sample/aboutImage.png'
-import linkedinIcon from '../assests/icons8-linkedin-50.png'; 
-import githubIcon from '../assests/githubImage.png';
-import resumeFile from '../assests/Pradeep-Resume.pdf'; // Import your resume file
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../Css/About.css";
+import AboutImage from "../assests/sample/aboutImage.png";
+import linkedinIcon from "../assests/icons8-linkedin-50.png";
+import githubIcon from "../assests/githubImage.png";
+import resumeFile from "../assests/Pradeep-Resume.pdf"; // Import your resume file
+import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
+import { Typewriter } from "react-simple-typewriter";
 
 function About() {
-
   // Function to handle the download
   const handleDownload = () => {
-    // Create a new anchor element
-    const link = document.createElement('a');
-    // Set the href attribute to the URL of the resume file
+    const link = document.createElement("a");
+
     link.href = resumeFile;
-    // Set the download attribute to force download
-    link.setAttribute('download', 'Pradeep-Resume.pdf');
-    // Append the anchor element to the body
+
+    link.setAttribute("download", "Pradeep-Resume.pdf");
+
     document.body.appendChild(link);
-    // Trigger a click event on the anchor element
+
     link.click();
-    // Remove the anchor element from the body
+
     document.body.removeChild(link);
   };
 
   return (
-    <section id="about" className="about-section">
-      <div className="about-content">
-        <div>
-          <img className='aboutImg' src={AboutImage} alt="avatar" />
+    <>
+    <div className="ABout_Row ">
+      <Row  id="about">
+        <Col sm={0} md={2} lg={2} xl={2}>
+          {" "}
+        </Col>
+
+        <Col sm={12} md={4} lg={4} xl={4}>
+          {" "}
+          <div className="text-container">
+            <div className="about-content">
+              <div >
+                <div
+                  className="About_Hello line"
+                  style={{ fontSize: "60px",marginRight:'80px' }}
+                >
+                  Hello !!
+                </div>
+                <div className="About_Hello line" style={{ fontSize: "50px" ,backgroundColor:"grey"}}>
+                  I'm <span className="About_Pradeep">Pradeep.</span>
+                </div>
+                <div className="About_Hello line" style={{ fontSize: "20px" ,backgroundColor:"grey"}}>
+                  Mern Stack Developer
+                </div>
+                <div className="About_Hello line" >
+              
+              <button className="aboutButton About_Hello line" onClick={handleDownload}>
+                Download CV
+              </button>
+          
+              <button className="aboutContactButton About_Hello line">
+                <Link to="/contact">Contact</Link>
+              </button>
+              </div>
+              <div className="line">
+              <div style={{ whiteSpace: "pre-wrap" }}  className='About_Details'>
+          <Typewriter
+         
+        
+            words={[
+              `Enthusiastic MERN stack developer with foundational knowledge in MongoDB, Express.js, React, and Node.js. Recently completed hands-on projects that involved developing full-stack applications, including building responsive user interfaces with React, creating RESTful APIs with Express.js, and managing data with MongoDB. Eager to apply my skills in a professional setting and contribute to dynamic development teams with a strong commitment to learning and growth.`
+            ]}
+            loop={1}
+            cursor
+            cursorStyle="|"
+            typeSpeed={50}
+            deleteSpeed={0}
+            delaySpeed={1000}
+          />
         </div>
-        <div>
-        <div className="text-container">
-          <h2>Pradeep</h2>
-          <p>
-            Full Stack Developer 
-          </p>
-          <div>
-            {/* Attach handleDownload function to the onClick event */}
-            <button className='aboutButton' onClick={handleDownload}>
-              Download CV
-            </button>
-            <button className='aboutContactButton'>
-              <Link to="/contact">Contact</Link> 
-            </button>
+
+              </div>
+              </div>
+            </div>
+
+            
+            <div className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/pradeep-s-8512a72a3/"
+                rel="noopener noreferrer"
+              >
+                <img className="Aboutlogo" src={linkedinIcon} alt="LinkedIn" />
+              </a>
+              <a href="https://github.com/ravana09" rel="noopener noreferrer">
+                <img className="Aboutlogo" src={githubIcon} alt="GitHub" />
+              </a>
+            </div>
           </div>
-          <div className="social-icons">
-            <a href="https://www.linkedin.com/in/pradeep-s-8512a72a3/"  rel="noopener noreferrer">
-              <img className='Aboutlogo' src={linkedinIcon} alt="LinkedIn" />
-            </a>
-            <a href="https://github.com/ravana09"  rel="noopener noreferrer">
-              <img className='Aboutlogo' src={githubIcon} alt="GitHub" />
-            </a>
-          </div>
-        </div>
-        </div>
+        </Col>
+        <Col sm={12} md={4} lg={4} xl={4}>
+          <img className="aboutImg" src={AboutImage} alt="avatar" />
+        </Col>
+        <Col sm={0} md={2} lg={2} xl={2}></Col>
+      </Row>
       </div>
-    </section>
+    </>
   );
 }
 
 export default About;
-
-
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll"; // Import ScrollLink from react-scroll
-import "../App.css";
+import "../Css/Header.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 
 function Header() {
   return (
-    <header className="header">
-      {/* <h1 className="header-h1">Pradeep.S</h1> */}
+    <>
+  
+    <Navbar expand="lg"  className="Header_Body" fixed="top" >
+        <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          
+          <Nav className="ms-auto">
+          <ScrollLink to="about" smooth={true} duration={100} className="nav-link" style={{color:"white",fontSize:"larger"}}>
+            Home
+            </ScrollLink>
+            <ScrollLink to="experience" smooth={true} duration={100} className="nav-link" style={{color:"white",fontSize:"larger"}}>
+              About me
+            </ScrollLink>
+            <ScrollLink to="skill" smooth={true} duration={100} className="nav-link" style={{color:"white",fontSize:"larger"}}>
+              Skill
+            </ScrollLink>
+            <ScrollLink to="projects" smooth={true} duration={100} className="nav-link" style={{color:"white",fontSize:"larger"}}>
+              Projects
+            </ScrollLink>
+            <ScrollLink to="contact" smooth={true} duration={100} className="nav-link" style={{color:"white",fontSize:"larger"}}>
+              Contact
+            </ScrollLink>
+            
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+     {/* <header className="header">
+    
       <nav>
         <ul>
           <li>
-            <Link to="/about">Home</Link>
+          <ScrollLink to="about" smooth={true} duration={500}>
+            Home
+            </ScrollLink>
           </li>
        
           <li>
@@ -21,26 +53,28 @@ function Header() {
             </ScrollLink>
           </li>
           <li>
-            {/* Replace Link with ScrollLink */}
+            
             <ScrollLink to="skill" smooth={true} duration={500}>
               Skill
             </ScrollLink>
           </li>
           <li>
-            {/* Replace Link with ScrollLink */}
+            
             <ScrollLink to="projects" smooth={true} duration={500}>
               Projects
             </ScrollLink>
           </li>
           <li>
-            {/* Replace Link with ScrollLink */}
+            
             <ScrollLink to="contact" smooth={true} duration={500}>
               Contact
             </ScrollLink>
           </li>
         </ul>
       </nav>
-    </header>
+    </header> */}
+    </>
+   
   );
 }
 
